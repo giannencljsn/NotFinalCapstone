5f469292d86a4d44b03cb209006f6926e6189275
Gianne Nicole Juson <73353587+giannencljsn@users.noreply.github.com>
2023-02-05 15:57:12 +0800

    Add files via upload

---
+++ user_page.php
@@ -0,0 +1,47 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>user page</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="preload.css">
+
+</head>
+<body>
+
+<div id="preloader">
+   <div class="spinner"></div>
+   </div>
+   
+<div class="container">
+
+   <div class="content">
+      <h3>hi, <span>user</span></h3>
+      <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
+      <p>this is an user page</p>
+      <a href="login_form.php" class="btn">login</a>
+      <a href="register_form.php" class="btn">register</a>
+      <a href="logout.php" class="btn">logout</a>
+   </div>
+
+</div>
+<script src="preload.js"></script>
+
+</body>
+</html>
